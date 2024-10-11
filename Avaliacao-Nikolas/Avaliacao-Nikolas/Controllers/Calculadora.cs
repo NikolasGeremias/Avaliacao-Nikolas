67fe08ac9e60d3cccd245336d016a215dafc8742
Nikolas Geremias <nikolasgeremias27@gmail.com>
2024-10-10 23:44:22 -0300

    commit

---
+++ Avaliacao-Nikolas/Avaliacao-Nikolas/Controllers/Calculadora.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Avaliacao_Nikolas.Services;
+
+namespace Avaliacao_Nikolas.Controllers
+{
+    public class Calculadora : Controller
+    {
+        private readonly CalculadoraService _calculadoraService;
+
+        public Calculadora()
+        {
+            _calculadoraService = new CalculadoraService();
+        }
+
+        public IActionResult RealizarSoma(int a, int b)
+        {
+            var resultado = _calculadoraService.Somar(a, b);
+            return View("Resultado", $"O resultado da soma é: {resultado}");
+        }
+
+        public IActionResult RealizarSubtracao(int a, int b)
+        {
+            var resultado = _calculadoraService.Subtrair(a, b);
+            return View("Resultado", $"O resultado da subtração é: {resultado}");
+        }
+
+        public IActionResult RealizarMultiplicacao(int a, int b)
+        {
+            var resultado = _calculadoraService.Multiplicar(a, b);
+            return View("Resultado", $"O resultado da multiplicação é: {resultado}");
+        }
+
+        public IActionResult RealizarDivisao(int a, int b)
+        {
+            try
+            {
+                var resultado = _calculadoraService.Dividir(a, b);
+                return View("Resultado", $"O resultado da divisão é: {resultado}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                return View("Resultado", ex.Message);
+            }
+        }
+    }
+}
---
+++ Avaliacao-Nikolas/Avaliacao-Nikolas/Services/CalculadoraService.cs
@@ -0,0 +1,13 @@
+﻿namespace Avaliacao_Nikolas.Services
+{
+    public class CalculadoraService
+    {
+        public int Somar(int a, int b) => a + b;
+
+        public int Subtrair(int a, int b) => a - b;
+
+        public int Multiplicar(int a, int b) => a * b;
+
+        public double Dividir(int a, int b) => b != 0 ? (double)a / b : throw new DivideByZeroException("Divisão por zero não é permitida.");
+    }
+}
